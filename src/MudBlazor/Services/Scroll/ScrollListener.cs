e6dd6d166353cc8774ffec469eea05262018f182
Porkopek <porkopek@users.noreply.github.com>
2020-12-20 04:08:17 +0000

    Added ScrollListener

---
+++ src/MudBlazor/Services/Scroll/ScrollListener.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+using MudBlazor.Services.Scroll;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MudBlazor
+{
+    public interface IScrollListener
+    {
+        ElementReference Ref { get; set; }
+
+        event EventHandler<ScrollEventArgs> OnScroll;
+    }
+
+    public class ScrollListener : IScrollListener
+
+    {
+        private readonly IJSRuntime _js;
+
+        public ElementReference Ref { get; set; }
+
+        public ScrollListener(IJSRuntime js)
+        {
+            _js = js;
+        }
+
+        private EventHandler<ScrollEventArgs> _onScroll;
+
+        /// <summary>
+        /// Subscribe to the browser onScroll event
+        /// </summary>
+        public event EventHandler<ScrollEventArgs> OnScroll
+        {
+
+            add => Subscribe(value);
+            remove => Unsubscribe(value);
+        }
+
+
+        private void Subscribe(EventHandler<ScrollEventArgs> value)
+        {
+           
+            if (_onScroll == null)
+            {
+                Task.Run(async () => await Start());
+            }
+            _onScroll += value;
+        }
+
+        private void Unsubscribe(EventHandler<ScrollEventArgs> value)
+        {
+            _onScroll -= value;
+            if (_onScroll == null)
+            {
+                Cancel().ConfigureAwait(false);
+            }
+        }
+
+
+        /// <summary>
+        /// ivoked in JS, in scroll-listener.js
+        /// </summary>
+        /// <param name="e">The scroll event args</param>
+        [JSInvokable]
+        public void RaiseOnScroll(ScrollEventArgs e)
+        {
+
+            _onScroll?.Invoke(this, e);
+        }
+
+
+        private async ValueTask<bool> Start() =>
+            await _js
+            .InvokeAsync<bool>
+            ("scrollListener.listenForScroll",
+                DotNetObjectReference.Create(this), 
+                Ref);
+
+
+        private async ValueTask Cancel()
+        {
+            try
+            {
+                await _js.InvokeVoidAsync("scrollListener.cancelListener");
+            }
+            catch (Exception)
+            {
+                /* ignore */
+            }
+        }
+    }
+}
---
+++ src/MudBlazor/TScripts/scroll-listener.js
@@ -0,0 +1,42 @@
+﻿window.scrollListener = {
+  throttleScrollHandlerId: -1,
+  dotnet: undefined,
+
+  listenForScroll: function (dotnetRef, element) {
+    this.element = element;
+    this.dotnet = dotnetRef;
+    this.element.addEventListener(
+      "scroll",
+      this.throttleScrollHandler.bind(this),
+      false
+    );
+
+    this.scrollHandler();
+  },
+
+  throttleScrollHandler: function () {
+    clearTimeout(this.throttleScrollHandlerId);
+
+    this.throttleScrollHandlerId = window.setTimeout(
+      this.scrollHandler.bind(this),
+      300
+    );
+  },
+
+  scrollHandler: function () {
+      try {
+          console.log("x: " + window.scrollX, "y: " + window.scrollY)
+      this.dotnet.invokeMethodAsync("RaiseOnScroll", {
+        x: window.scrollX,
+        y: window.scrollY,
+      });
+    } catch (error) {
+      console.log("[MudBlazor] Error in scrollHandler:", { error });
+    }
+  },
+
+  cancelListener: function () {
+    //console.log("[MudBlazor] cancelListener");
+    this.element.removeEventListener("scroll", this.throttleScrollHandler);
+  },
+};
